021f3711251b640909aeaf6139e7217325087947
alexandruvlad15 <alexandru.vlad1504@stud.aero.upb.ro>
2024-09-27 20:11:57 +0300

    Buttons&Labels

--- Pk.java
+++ Pk.java
@@ -3,13 +3,19 @@ import javax.swing.JPanel;
 import javax.swing.BorderFactory;
 import java.awt.GridLayout;
 import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 public class GUI {
     public GUI() {
         JFrame frame = new JFrame();
+        JButton button=new JButton("Click");
+        JLabel label=new JLabel("Nr clicks: 0");
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Our GUI");
